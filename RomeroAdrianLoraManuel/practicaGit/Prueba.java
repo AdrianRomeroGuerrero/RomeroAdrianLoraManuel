c7cea42708e91b4bcdcce5b469f0fa1d9bbc27e2
AdrianRomeroGuerrero <adrirogue@gmail.com>
2022-05-09 13:34:59 +0200

    Prueba con Manuel Lora

---
+++ RomeroAdrianLoraManuel/practicaGit/Prueba.java
@@ -0,0 +1,14 @@
+package practicaGit;
+
+public class Prueba {
+
+	public static void main(String[] args) {
+		int numero = 1;
+		int numeroDos = 2;
+		int resultadoSuma = numero + numeroDos;
+		
+		System.out.println("La suma del número " + numero + " + " + numeroDos + " es = " + resultadoSuma);
+
+	}
+
+}
